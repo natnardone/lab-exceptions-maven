136c029edd1dd104d95b3f7cdacef2d1460a135a
Natalie Nardone <nardonen@grinnell.edu>
2024-09-24 09:47:03 -0500

    Add instructions to QR

--- src/main/java/edu/grinnell/csc207/main/QR.java
+++ src/main/java/edu/grinnell/csc207/main/QR.java
@@ -27,7 +27,22 @@ public class QR {
     PrintWriter pen = new PrintWriter(System.out, true);
     BufferedReader eyes = new BufferedReader(new InputStreamReader(System.in));
 
-    // ...
+    pen.print("Hi! I'm here to help you understand quadratic functions.\n");
+    pen.print("Please enter a (the coefficient of the squared term): ");
+    pen.flush();
+    String a = eyes.readLine();
+    pen.print("Please enter b (the coefficient of the linear term): ");
+    pen.flush();
+    String b = eyes.readLine();
+    pen.print("Please enter c (the constant): ");
+    pen.flush();
+    String c = eyes.readLine();
+
+    Quadratic quad = new Quadratic(Double.parseDouble(a), Double.parseDouble(b), Double.parseDouble(c));
+    double smallroot = quad.smallerRoot();
+    pen.print("The smaller root of "+ quad.toString() + " is " + smallroot + ".\n");
+    pen.print("Experimentally, " + quad.toString(smallroot) + " = " + quad.evaluate(smallroot) + ".\n");
+
 
     eyes.close();
     pen.close();
